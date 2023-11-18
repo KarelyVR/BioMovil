52c8626dbba9bf17bdc873ffd06cfc6fbfa14218
Lineett <86264742+Lineett@users.noreply.github.com>
2023-11-17 19:07:52 -0600

    notificaciones
    boton de notificaciones funcional

---
+++ lib/pantalla_ajustes/notificaciones/home_page.dart
@@ -0,0 +1,110 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:biomovil/pantalla_ajustes/notificaciones/Services/notifi_service.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late Timer _timer;
+  int _notificationId = 0;
+  List<Map<String, String>> _notificationMessages = [
+    {
+      'title': '¡Tu safari está por comenzar!',
+      'body': 'El safari comenzará en 10 minutos.'
+    },
+    {
+      'title': '¡Show de animales!',
+      'body': 'No te pierdas nuestro show en vivo'
+    },
+    {
+      'title': '¡Jornada de Elefantes y Leones!',
+      'body':
+          'Sumérgete en el mundo de los elefantes y deslúmbrate con la majestuosidad de los leones'
+    },
+    {
+      'title': '¡Explora la historia en vivo!',
+      'body':
+          'Descubre reliquias sorprendentes y sumérgete en la historia en nuestro museo.'
+    },
+    {
+      'title': '¡Encuentro multicolor: Tucanes y Mandriles!',
+      'body':
+          'Sumérgete en la diversidad de colores con nuestros tucanes y mandriles.'
+    },
+    {
+      'title': '¡Explora la vida salvaje: Coyotes y Liebres!',
+      'body':
+          'Explora su hábitat y descubre su fascinante mundo en nuestro santuario de la vida animal'
+    },
+    {
+      'title': '¡No olvides visitar nuestra galeria!',
+      'body':
+          'Explora la magia de la vida salvaje a través de nuestras fotografías.'
+    },
+    {
+      'title': '¡Vuelo de color: Aves Exóticas!',
+      'body':
+          'Nuestra atracción te lleva a un viaje cautivador a través de las especies más exóticas y fascinantes.'
+    },
+  ];
+
+  int _currentMessageIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _startTimer();
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  void _startTimer() {
+    _timer = Timer.periodic(const Duration(seconds: 70), (timer) {
+      _sendNotification();
+    });
+  }
+
+  void _sendNotification() {
+    setState(() {
+      _notificationId++;
+      // Cambiar al siguiente mensaje de la lista
+      _currentMessageIndex =
+          (_currentMessageIndex + 1) % _notificationMessages.length;
+    });
+
+    final message = _notificationMessages[_currentMessageIndex];
+    NotificationService().showNotification(
+      id: _notificationId,
+      title: message['title']!,
+      body: message['body']!,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Enviar notificaciones'),
+          onPressed: () {
+            _sendNotification();
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pantalla_ajustes/notificaciones/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'Services/notifi_service.dart';
+
+import 'home_page.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  NotificationService().initNotification();
+  runApp(const notificaciones());
+}
+
+class notificaciones extends StatelessWidget {
+  const notificaciones({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Notificaciones',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const MyHomePage(title: 'Notificaciones'),
+    );
+  }
+}
